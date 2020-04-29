72f45f816306f8a00aed6af4e12f99e520d2b39c
scdow <920301714@qq.com>
2020-04-30 02:53:33 +0800

    Create Assignment5

---
+++ app/src/main/java/com/example/compx202_assignment5_3171034/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.compx202_assignment5_3171034;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
